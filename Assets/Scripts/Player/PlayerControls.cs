f44c527558f86fc012c767486fe31d58db0110c2
Reece Roberts <reece.roberts288@gmail.com>
2026-07-18 13:02:04 +0100

    Player Movement setup
    The player is able to move in all directions and can do so in a smooth way

--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -3,10 +3,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
-    private Rigidbody2D playerRb;
-    private Vector2 movementInput; 
+    private Rigidbody2D playerRb; //Player Rigid body 2D
+    private Vector2 movementInput; // Player Movement Function
 
-    
+    private Vector2 smoothMoveInput; //Makes player movement more smooth
+    private Vector2 movementInputSmoothVelocity; // Tracks current speed of smooth movement
+
+    //Player speed multiplyer
+    [SerializeField]private float playerSpeed = 5f; 
+
+
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody2D>();
@@ -14,9 +20,18 @@ public class PlayerControls : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRb.linearVelocity = movementInput;
+        //Track Players current movementInput to allow for gradual
+        //smoother stopping of the player when there is no input over time
+        smoothMoveInput = Vector2.SmoothDamp(smoothMoveInput, 
+            movementInput, 
+            ref movementInputSmoothVelocity,
+            0.1f);//When to trigger funcion 
+
+        //Player Rigidbody2D will move based on the input and multiply by the float speed
+        playerRb.linearVelocity = smoothMoveInput * playerSpeed;
     }
 
+    //Move player based on NewInputSystem
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
